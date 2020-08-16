d20096b292c050a8ae9007e1c1e14cb23eafcd1c
PiggyPiglet <noreply@piggypiglet.me>
2020-08-16 20:38:26 +0800

    started working on loot format

---
+++ src/main/java/me/piggypiglet/infectedportals/portal/PortalLootPlacer.java
@@ -0,0 +1,17 @@
+package me.piggypiglet.infectedportals.portal;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+// ------------------------------
+// Copyright (c) PiggyPiglet 2020
+// https://www.piggypiglet.me
+// ------------------------------
+public final class PortalLootPlacer {
+    public void place() {
+        final ItemStack stack = new ItemStack(Material.DIAMOND_SWORD, 1);
+        final ItemMeta meta = stack.getItemMeta();
+        meta.add
+    }
+}
